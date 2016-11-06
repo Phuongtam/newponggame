80569a8349a7eab6966fb2a17652d10694601c54
Trung Ca <Windows 8.1 Gamer@WIN-0BIFCMNNGIA>
2016-11-06 15:06:52 +0700

    Exchange Score in GameOver fix #13

--- PongGame/src/PongPanel.java
+++ PongGame/src/PongPanel.java
@@ -252,8 +252,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			// Draw scores
 			// TODO Set Blue color
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
-			g.drawString(String.valueOf(playerTwoScore), 100, 100);
-			g.drawString(String.valueOf(playerOneScore), 400, 100);
+			g.drawString(String.valueOf(playerOneScore), 100, 100);
+			g.drawString(String.valueOf(playerTwoScore), 400, 100);
 
 			// Draw the winner name
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
