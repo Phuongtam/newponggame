c813a4b4bdf458f608bbdeb2271ca9bfaf6e2e15
TamNguyen <phuongtam128@gmail.com>
2016-11-06 13:33:59 +0700

    fix #7 - separating line is green

--- PongGame/src/MainGameUI.java
+++ PongGame/src/MainGameUI.java
@@ -17,7 +17,7 @@ import javax.swing.JFrame;
  */
 public class MainGameUI extends JFrame{
 	private static final int _HEIGHT = 500;
-	private static final int _WIDTH = 600;
+	private static final int _WIDTH = 500;
 	private PongPanel pongPanel;
 	
 	public MainGameUI(){
